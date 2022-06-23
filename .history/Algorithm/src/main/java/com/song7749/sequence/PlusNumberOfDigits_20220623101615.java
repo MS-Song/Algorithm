ce80f95e8bf4a150de0571a6bc640e504be08cbd
송민수 <d200107@kbfg.com>
2022-06-23 18:07:11 +0900

    구조변경

---
+++ .history/Algorithm/src/main/java/com/song7749/sequence/PlusNumberOfDigits_20220623101615.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PlusNumberOfDigits {
+
+	public static void main(String[] args) {
+    
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+
+  		do {
+            answer += n % 10;
+            n /=10;
+        } while(n > 0) ;
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println(answer);
+        return answer;
+    }
+}
