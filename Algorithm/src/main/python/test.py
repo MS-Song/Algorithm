ef90df667f8735bc3a458af48c14c675b4107090
MS Song <song7749@gmail.com>
2024-03-03 20:20:39 +0900

    개발완료

--- Algorithm/src/main/python/test.py
+++ Algorithm/src/main/python/test.py
@@ -12,20 +12,20 @@ print ("------------")
 
 blank_draw = " "
 star_draw = "*"
-max = 15
+max = 11
 
 for i in range(1,max-1):
     star = i + (i-1)
     if(star > max):
-        star = round(max/3)
+        star = int(max/3)
 
     max_blank = max - star
     draw = ""
-    for j in range(0,round(max_blank/2)):
+    for j in range(0,int(max_blank/2)):
         draw +=blank_draw
     for j in range(0,star):
         draw +=star_draw
-    for j in range(0,round(max_blank/2)):
+    for j in range(0,int(max_blank/2)):
         draw +=blank_draw
     print(draw)
                 
